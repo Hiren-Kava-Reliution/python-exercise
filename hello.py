03bd2559b7402f534cb061ae26b2f98681df70d4
hiren-kava-reliution <hirenk.reliution@gmail.com>
2026-02-26 15:27:35 +0530

    Loop Program

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 n = 1
 for i in range(1, 5):
-    for j in range(1, 5):
-        print(n, end=" ")
+    for j in range(1, 6):
+        if i % 2 == 1:
+            print(n, end=" ")
+        elif i % 2 == 0:
+            print((i * 5) - j + 1, end=" ")
         n += 1
     print()
